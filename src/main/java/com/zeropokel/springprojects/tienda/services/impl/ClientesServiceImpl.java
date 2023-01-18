ecbdf2b2a4bfa8a15d53218057c2fada27bcc7fa
Miguel Angel Femenia <mfemvaz2906@g.educaand.es>
2023-01-18 12:03:03 +0100

    Añadido repositorios a Cliente

--- src/main/java/com/zeropokel/springprojects/tienda/services/impl/ClientesServiceImpl.java
+++ src/main/java/com/zeropokel/springprojects/tienda/services/impl/ClientesServiceImpl.java
@@ -1,6 +1,6 @@
 package com.zeropokel.springprojects.tienda.services.impl;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,12 +8,46 @@ import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import com.zeropokel.springprojects.tienda.dao.ClientesDAO;
 import com.zeropokel.springprojects.tienda.model.Cliente;
+import com.zeropokel.springprojects.tienda.repository.ClienteRepository;
 import com.zeropokel.springprojects.tienda.services.ClientesService;
 
 @Service
 public class ClientesServiceImpl implements ClientesService {
+
+    @Autowired
+    ClienteRepository repository;
+
+    @Override
+    public Page<Cliente> findAll(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    @Override
+    public Cliente findByID(int codigo) {
+        Optional<Cliente> findById = repository.findById(codigo);
+        if(findById != null){
+            return findById.get();
+        }
+        return null;
+    }
+
+    @Override
+    public void insert(Cliente cliente) {
+        repository.save(cliente);   
+    }
+
+    @Override
+    public void update(Cliente cliente) {
+        repository.save(cliente);
+    }
+
+    @Override
+    public void delete(int codigo) {
+        repository.deleteById(codigo);
+    }
+
+    /* CON DAO
 
     @Autowired
     ClientesDAO clientesDAO;
@@ -42,4 +76,5 @@ public class ClientesServiceImpl implements ClientesService {
     public void delete(int codigo){
         clientesDAO.delete(codigo);
     }
+    */
 }
